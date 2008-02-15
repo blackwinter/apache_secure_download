b65f0253cc8672cdb9ad2016f2883d6b826d173c
Jens Wille <jens.wille@uni-koeln.de>
2008-02-15 17:04:19 +0000

    <apache_secure_download> lib/apache_secure_download.rb: Simplified a bit...

--- lib/apache_secure_download.rb
+++ lib/apache_secure_download.rb
@@ -5,26 +5,20 @@ module Apache
   class SecureDownload
 
     def initialize(secret)
-      raise ArgumentError, 'secret string missing' unless secret
-
-      @secret = secret
+      raise ArgumentError, 'secret string missing' unless @secret = secret
     end
 
     def check_access(request)
-      path      = request.uri
-      timestamp = request.param('timestamp')
-      token     = request.param('token')
-
-      return FORBIDDEN if timestamp.to_i < Time.now.to_i
-      return FORBIDDEN if token != compute_token(path, timestamp)
+      return FORBIDDEN if request.param('timestamp').to_i < Time.now.to_i
+      return FORBIDDEN if request.param('token') != compute_token(request)
 
       return OK
     end
 
     private
 
-    def compute_token(path, timestamp)
-      Digest::SHA1.hexdigest(@secret + path + timestamp)
+    def compute_token(request)
+      Digest::SHA1.hexdigest(@secret + request.uri + request.param('timestamp'))
     end
 
   end
